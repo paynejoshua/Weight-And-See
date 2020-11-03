3f9df32b6b4f0c34b2d1303d8d7bba621b9998c0
Josh Payne <paynejoshua1@gmail.com>
2020-11-03 10:33:05 -0800

    updated html-routes in attempt to solve routing issue

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@ let path = require("path")
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    res.sendFile(path.join(__dirname, "/exercise.html"))
 })
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
+    res.sendFile(path.join(__dirname, "/stats.html"))
 })
 
 module.exports = router
