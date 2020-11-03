6b3b3a874bd40c252715816f7e7a63e1a8edba2d
Josh Payne <paynejoshua1@gmail.com>
2020-11-03 10:23:13 -0800

    deploying app

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/WeightTracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
